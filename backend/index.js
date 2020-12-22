cbb46dac2efbd07872cf9887f561123f93ec7715
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-12-21 21:41:23 -0800

    first commit

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const chance = require('chance');
+const uuid = require('uuid').v4;
+
+const levels = ['warn', 'error', 'status'];
+
+const randomLevel = () => {
+  return levels[Math.floor(Math.random() * 3)];
+};
+
+const randomGenerator = () => {
+  return {
+    level: randomLevel(),
+    message: chance().sentence({ words: 5 }),
+    timestamp: new Date(Date.now()).toUTCString(),
+    id: uuid(),
+  };
+};
+
+setInterval(() => {
+  console.log(randomGenerator());
+}, 2000);
