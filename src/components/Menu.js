03291f01bfb0046d75f392f56d630cdeaadacead
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-01 13:10:14 -0800

    added scroll listener

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,24 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import style from './Menu.module.scss';
-import Button from '@material-ui/core/Button';
-import PlayPauseIcons from './PlayPauseIcons';
-import DeleteIcon from '@material-ui/icons/Delete';
-import Select from './Select';
-import Dialogue from './Dialogue';
 import { useDashboard } from '../context';
-import Chart from './Chart';
+import FullMenu from './FullMenu';
+import MiniMenu from './MiniMenu';
 
 function Menu() {
-  const {
-    setIsRunning,
-    isRunning,
-    state,
-    setSnackbar,
-    select,
-    msgsInView,
-  } = useDashboard();
+  const { setIsRunning, isRunning, state, setSnackbar } = useDashboard();
   const [plural, setPlural] = useState('messages');
-  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    window.onscroll = () => {
+      setOffset(window.pageYOffset);
+    };
+  }, []);
+
+  console.log(offset);
 
   useEffect(() => {
     if (state.length === 1) {
@@ -34,47 +30,15 @@ function Menu() {
     let message = isRunning ? 'Paused' : 'Started';
     setSnackbar(message);
   }
-  
+
   return (
-    <div className={style.main}>
-      <div className={style.left}>
-        {showDeleteConfirmation && (
-          <Dialogue
-            inMenu={true}
-            setShowDeleteConfirmation={setShowDeleteConfirmation}
-          />
-        )}
-        <div className={style.status}>
-          <h2>Status: {isRunning ? 'Running' : 'Paused'}</h2>
-          <PlayPauseIcons />
-        </div>
-        <Button
-          onClick={(e) => toggleIsRunning()}
-          variant='contained'
-          style={{ marginRight: '1rem' }}
-        >
-          {isRunning ? 'Pause' : 'Start'}
-        </Button>
-        <Button
-          onClick={(e) => setShowDeleteConfirmation(true)}
-          variant='contained'
-          startIcon={<DeleteIcon />}
-          style={{ background: '#AA647B' }}
-        >
-          Delete all
-        </Button>
-        <br />
-        <p>Show only:</p>
-        <Select />
-      </div>
-      <div className={style.right}>
-        <Chart />
-        <p>
-          Displaying {msgsInView.length}{' '}
-          {select !== 'view all' && `of ${state.length} total`} {plural}
-        </p>
-      </div>
-    </div>
+    <>
+      {offset < 1 ? (
+        <FullMenu toggleIsRunning={toggleIsRunning} plural={plural} />
+      ) : (
+        <MiniMenu toggleIsRunning={toggleIsRunning} plural={plural} />
+      )}
+    </>
   );
 }
 
--- src/helpers/styles.js
+++ src/helpers/styles.js
@@ -13,40 +13,7 @@ export const useStyles = makeStyles((theme) => ({
       borderColor: 'red',
     },
   },
-  icon: {
-    fill: 'red',
-  },
   selectEmpty: {
     marginTop: theme.spacing(2),
-  },
-  root: {
-    width: 200,
-    "& .MuiOutlinedInput-input": {
-      color: "green"
-    },
-    "& .MuiInputLabel-root": {
-      color: "green"
-    },
-    "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-      borderColor: "green"
-    },
-    "&:hover .MuiOutlinedInput-input": {
-      color: "red"
-    },
-    "&:hover .MuiInputLabel-root": {
-      color: "red"
-    },
-    "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-      borderColor: "red"
-    },
-    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-input": {
-      color: "purple"
-    },
-    "& .MuiInputLabel-root.Mui-focused": {
-      color: "purple"
-    },
-    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
-      borderColor: "purple"
-    }
   }
 }));
