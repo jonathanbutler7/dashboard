6776e833036e3b8cd8f1e548cbf349c9759c5b55
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-08 10:06:22 -0800

    resolved delete issue

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -6,12 +6,12 @@ export function reducer(state, action) {
         allMessages: [...state.allMessages, action.payload],
       };
     case 'DELETE_ALL':
-      return { ...state, messages: [] };
+      return { ...state, allMessages: [], msgsInView: [] };
     case 'DELETE_ONE':
       let newMessages = state.allMessages.filter(
         (msg) => msg.id !== action.payload
       );
-      return { ...state, messages: newMessages };
+      return { ...state, allMessages: newMessages };
     case 'IS_RUNNING':
       return { ...state, isRunning: action.payload };
     case 'SET_SNACKBAR':
